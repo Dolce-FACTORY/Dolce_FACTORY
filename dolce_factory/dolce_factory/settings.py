1b4e16bba32f9384f8610f2f5ca53020de9242b0
bulldozer <loshka5647@gmail.com>
2023-11-25 11:37:37 +0200

    Small changes

--- dolce_factory/dolce_factory/settings.py
+++ dolce_factory/dolce_factory/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@5)5cz+qcc1_^ou+u9xy08w3j+!%ycaz@svg+51h&kl8hvmuh7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "main"
+    "main.apps.MainConfig"
 ]
 
 MIDDLEWARE = [
